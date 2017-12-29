10cf97fd025053058d3fc9aad6daa870a7255184
Dylan Humphreys <dylanjhumphreys@gmail.com>
2017-12-29 21:11:29 +0000

    this should make outputOnly work

--- heckle.go
+++ heckle.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 	"log/syslog"
 	"regexp"
+	//"encoding/csv"
 )
 
 // use to "test" exec_cmd will output the command passed to it, rather than actually execute it
@@ -17,6 +18,7 @@ import (
 // const outputOnly=false
 
 var version string
+var outputOnly bool
 
 func exec_cmd(cmd string, outputonly bool) string {
 	if !outputonly  {
@@ -32,62 +34,34 @@ func exec_cmd(cmd string, outputonly bool) string {
 		// trim off the CR at the end of any output
 		return output
 	} else {
-		return cmd	
+		return cmd
 	}
 }
 
-func main() {
-	vmtemplatePtr		:= flag.String("template","","The Name of the XenServer template to use")
-	vmnamePtr 			:= flag.String("name","blah","The name of the VM to create")
-	vmcpusPtr 			:= flag.Int("cpus",1,"the number of vCPUs to assign to the VM")
-	vmemoryPtr	 		:= flag.String("memory","1GiB","The number of MEGABYTES RAM to assign to the VM")
-	vmdisksizePtr		:= flag.String("disksize","10GiB","The number of GiB to allocate to the disk")
-	vmnetworkPtr		:= flag.String("network","","The name of the network to connect the vm to")
-	vmisoPtr			:= flag.String("iso","","The name of the ISO from which to first-time-boot the vm")
-	vmoutputonlyPtr		:= flag.Bool("outputonly",false,"If set it will only output the commands it would execute, naturally without the correct parameter values set.")
-	vmversionPtr		:= flag.Bool("version",false,"show the version and date of the build")
-	vmstartPtr			:= flag.Bool("start",false,"If set it will start the vm once created")
-
-	flag.Parse()
-
+func createvm(template string, name string, cpus int,memory string, disksize string,network string, iso string) string {
 	var vm_unwantedoutput =""
-	
-	outputOnly := *vmoutputonlyPtr
-
-	// if *vmstartPtr>0 {
-	// 	fmt.Println("start is NOT 0")
-	// 	os.Exit(0)
-	// }
-	if *vmoutputonlyPtr {
-		fmt.Println("outputonly commands will not execute")
-	}
-
-	if *vmversionPtr {
-		fmt.Println("version " + version)
-		os.Exit(0)
+	if outputOnly {
+		fmt.Println("outputting only")
+	} else {
+		fmt.Println("executing commands")
 	}
 	
-	logwriter, e := syslog.New(syslog.LOG_NOTICE, "heckle")
-    if e == nil {
-        log.SetOutput(logwriter)
-    }
-
 	// STEP 0 validate the inputs
 	sizeCheck := regexp.MustCompile(`[0-9]+[GMK]iB`)
-	matches := sizeCheck.FindAllString(*vmemoryPtr,-1)
+	matches := sizeCheck.FindAllString(memory,-1)
 	if matches == nil {
 		fmt.Println("Memory must be a number followed by (G|M|K)iB")
 		os.Exit(1)
 	}
 
 
-	matches = sizeCheck.FindAllString(*vmdisksizePtr,-1)
+	matches = sizeCheck.FindAllString(disksize,-1)
 	if matches == nil {
 		fmt.Println("Disk size must be a number followed by (G|M|K)iB")
 		os.Exit(1)
 	}
 
-	if *vmcpusPtr < 1 {
+	if cpus < 1 {
 		fmt.Println("CPUs must be a positive integer")
 		os.Exit(1)
 	}
@@ -95,7 +69,7 @@ func main() {
 	// STEP 1
 	// create the VM with the required template
 	var vm_uuid =""
-	vm_uuid = exec_cmd("xe vm-install template=\"" + *vmtemplatePtr + "\" new-name-label=\"" + *vmnamePtr +"\"",outputOnly)
+	vm_uuid = exec_cmd("xe vm-install template=\"" + template + "\" new-name-label=\"" + name + "\"",outputOnly)
 	// once we've output the command, assign a dummy value to the variables we'll need later on, to make it look vaugely like a real run
 	if outputOnly {
 		fmt.Println(vm_uuid)
@@ -123,14 +97,14 @@ func main() {
 		vm_vdi_uuid="vm_vdi_uuid"
 	}
 	//xe vdi-resize uuid=[VDI uuid] disk-size=20GiB
-	vm_unwantedoutput = exec_cmd("xe vdi-resize uuid=" + vm_vdi_uuid + " disk-size=" + *vmdisksizePtr , outputOnly)
+	vm_unwantedoutput = exec_cmd("xe vdi-resize uuid=" + vm_vdi_uuid + " disk-size=" + disksize , outputOnly)
 	if outputOnly {
 		fmt.Println(vm_unwantedoutput)
 	}
 
 	// STEP 4 now we add a cd drive and "insert" the cd image
 	
-	vm_unwantedoutput = exec_cmd("xe vm-cd-add uuid=" + vm_uuid + " cd-name=\"" + *vmisoPtr + "\" device=3",outputOnly)
+	vm_unwantedoutput = exec_cmd("xe vm-cd-add uuid=" + vm_uuid + " cd-name=\"" + iso + "\" device=3",outputOnly)
 	if outputOnly {
 		fmt.Println(vm_unwantedoutput)
 	}
@@ -156,7 +130,7 @@ func main() {
 
 	// STEP 5 now on to setting up the network
 	//xe network-list --minimal name-label="CG 1072"	
-	var vm_network_uuid = exec_cmd("xe network-list --minimal name-label=\"" + *vmnetworkPtr + "\"", outputOnly)
+	var vm_network_uuid = exec_cmd("xe network-list --minimal name-label=\"" + network + "\"", outputOnly)
 	
 	if outputOnly {
 		fmt.Println(vm_network_uuid)
@@ -172,18 +146,18 @@ func main() {
 	// STEP 6 now set the RAM accordingly
 	// xe vm-memory-limits-set dynamic-max=VM MEMORY dynamic-min=VM MEMORY static-max=VM MEMORY static-min=VM MEMORY name-label="newVM"
 	vm_unwantedoutput = exec_cmd("xe vm-memory-limits-set dynamic-max=" + 
-				*vmemoryPtr + " dynamic-min=" + *vmemoryPtr + " static-max=" + *vmemoryPtr + 
-					" static-min=" + *vmemoryPtr + " uuid=" + vm_uuid, outputOnly)
+				memory + " dynamic-min=" + memory + " static-max=" + memory + 
+					" static-min=" + memory + " uuid=" + vm_uuid, outputOnly)
 	if outputOnly {	
 		fmt.Println(vm_unwantedoutput)
 	}
 
 	// STEP 7 set the number of VCPUs
-	vm_unwantedoutput = exec_cmd("xe vm-param-set uuid=" + vm_uuid + " platform:cores-per-socket=1 VCPUs-max=" + strconv.Itoa(*vmcpusPtr), outputOnly)
+	vm_unwantedoutput = exec_cmd("xe vm-param-set uuid=" + vm_uuid + " platform:cores-per-socket=1 VCPUs-max=" + strconv.Itoa(cpus), outputOnly)
 	if outputOnly {	
 		fmt.Println(vm_unwantedoutput)
 	}
-	vm_unwantedoutput = exec_cmd("xe vm-param-set uuid=" + vm_uuid + " platform:cores-per-socket=1 VCPUs-at-startup=" + strconv.Itoa(*vmcpusPtr), outputOnly)
+	vm_unwantedoutput = exec_cmd("xe vm-param-set uuid=" + vm_uuid + " platform:cores-per-socket=1 VCPUs-at-startup=" + strconv.Itoa(cpus), outputOnly)
 	if outputOnly {	
 		fmt.Println(vm_unwantedoutput)
 	}
@@ -194,11 +168,55 @@ func main() {
 	if outputOnly {	
 		fmt.Println(vm_unwantedoutput)
 	}
-	// READY!!!! 
+	
+	return vm_uuid
+
+	
+}
+
+func startvm(vm_uuid string) {
+	var vm_unwantedoutput string
+	vm_unwantedoutput = exec_cmd("xe vm-start uuid=" + vm_uuid, outputOnly)
+	if outputOnly {	
+		fmt.Println(vm_unwantedoutput)
+	}
+}
+
+func main() {
+	vmtemplatePtr		:= flag.String("template","","The Name of the XenServer template to use")
+	vmnamePtr 			:= flag.String("name","blah","The name of the VM to create")
+	vmcpusPtr 			:= flag.Int("cpus",1,"the number of vCPUs to assign to the VM")
+	vmemoryPtr	 		:= flag.String("memory","1GiB","The number of MEGABYTES RAM to assign to the VM")
+	vmdisksizePtr		:= flag.String("disksize","10GiB","The number of GiB to allocate to the disk")
+	vmnetworkPtr		:= flag.String("network","","The name of the network to connect the vm to")
+	vmisoPtr			:= flag.String("iso","","The name of the ISO from which to first-time-boot the vm")
+	vmversionPtr		:= flag.Bool("version",false,"show the version and date of the build")
+	vmstartPtr			:= flag.Bool("start",false,"If set it will start the vm once created")
+	// vmmanifest			:= flag.String("manifest","","A CSV file containing the template,name,cpus,memory,disksize,network,iso values for multiple hosts")
+	vmoutputonlyPtr		:= flag.Bool("outputonly",false,"If set it will only output the commands it would execute, naturally without the correct parameter values set.")
+
+	flag.Parse()
+	outputOnly = *vmoutputonlyPtr
+	logwriter, e := syslog.New(syslog.LOG_NOTICE, "heckle")
+
+	if outputOnly {
+		fmt.Println("outputonly commands will not execute")
+	}
+
+	if *vmversionPtr {
+		fmt.Println("version " + version)
+		os.Exit(0)
+	}
+	
+    if e == nil {
+        log.SetOutput(logwriter)
+    }
+
+    var vm_uuid string
+    
+    vm_uuid = createvm(*vmtemplatePtr, *vmnamePtr, *vmcpusPtr, *vmemoryPtr, *vmdisksizePtr, *vmnetworkPtr, *vmisoPtr)
+    // READY!!!! 
 	if *vmstartPtr {
-		vm_unwantedoutput = exec_cmd("xe vm-start uuid=" + vm_uuid, outputOnly)
-		if outputOnly {	
-			fmt.Println(vm_unwantedoutput)
-		}
+		startvm(vm_uuid)
 	}
 }
